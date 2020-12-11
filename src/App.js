5437bddcdca8c1adb19d9f16cab0804f40aad6fa
rrkkee015 <45933358+rrkkee015@users.noreply.github.com>
2020-12-11 21:17:09 +0900

    userInput Hooks TIL

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, { Component, useState } from "react";
 
 import './App.css';
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
-  return { value };
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      // validator(value) 함수의 결과는 App에 선언한 maxLen의 결과와 같다.
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  }
+  return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr. ");
+  // const maxLen = (value) => value.length <= 10;
+  const chk = (value) => !value.includes("@");
+  const name = useInput("Mr. ", chk);
 
   return (
     <div>
       <h1>Hello</h1>
-      <input placeholder="Name" value={name.value} />
+      <input placeholder="Name" {...name} />
+      {/* <input placeholder="Name" value={name.value} onChange={name.onChange} /> */}
     </div>
   )
 };
